df46c64f009d431422f259c05ac8089ce16d7829
Dhani Luthfi Ramadhani <Dhani@esrisa.co.id>
2018-10-17 05:51:29 +0700

    Add double back button to exit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, ToastController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
@@ -10,10 +10,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: [ 'app.scss' ]
 })
 export class AppComponent {
+  public counter=0;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    public toastCtrl: ToastController 
   ) {
     this.initializeApp();
   }
@@ -22,6 +25,25 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      this.platform.backButton.subscribe(() => {
+        if (this.counter == 0) {
+          this.counter++;
+          this.presentToast();
+          setTimeout(() => { this.counter = 0 }, 3000)
+        } else {
+          navigator['app'].exitApp();
+        }
+      }, 0)
     });
+  }
+
+  presentToast() {
+    let toast = this.toastCtrl.create({
+      message: "Press again to exit",
+      duration: 3000,
+      position: "middle"
+    });
+    toast.present();
   }
 }
